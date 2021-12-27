0d7ab93fbf357774b60ba3c229cf153ce06c5755
Maurice Alves <maurice.estevam@gmail.com>
2021-12-27 13:05:56 -0300

    Requisito - 4.

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-// Faça seu código aqui
 const app = require('express')();
 const http = require('http').createServer(app);
 const cors = require('cors');
 const moment = require('moment');
 const path = require('path');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -17,24 +16,33 @@ const io = require('socket.io')(http, {
 });
 
 app.use(cors());
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
 
-io.on('connection', (socket) => {
-   const id = socket.id.slice(0, 16);
-   io.emit('createNickname', id);
-    console.log(`Usuário conectado ${id}!`);
-    
-    const date = moment().format('DD-MM-yyyy HH:mm:ss A');
+const onUsers = {};
+const date = moment().format('DD-MM-yyyy HH:mm:ss A');
 
-    socket.on('message', ({ nickname, chatMessage }) => {
+io.on('connection', (socket) => {
+   onUsers[socket.id] = socket.id.slice(0, 16);
+    console.log(`Usuário conectado ${Object.values(onUsers)}!`);
+
+  socket.on('message', ({ nickname, chatMessage }) => {
       io.emit('message', `${date} - ${nickname}: ${chatMessage}`);
     });
+
+    socket.on('newName', (nickname) => {
+      onUsers[socket.id] = nickname;
+      io.emit('onlineUsersList', Object.values(onUsers));
+    });
+
     socket.on('disconnect', () => {
+      delete onUsers[socket.id];
+      io.emit('onlineUsersList', Object.values(onUsers));
       console.log('Desconectou!');
     });
+    io.emit('onlineUsersList', Object.values(onUsers));
 });
 
 app.get('/', (_req, res) => {
